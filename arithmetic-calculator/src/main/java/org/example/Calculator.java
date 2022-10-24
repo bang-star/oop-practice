0bb5974006375f7a504bfd1c7550ca54a9344aba
bang-star <bang77060986@gmail.com>
2022-10-25 00:44:44 +0900

    Update
    - Enum 클래스 + 추상화 메소드 방식이 아닌 인터페이스를 통한 구현체를 사용하는 방식으로 전환
    - Stream을 통한 메소드 처리

--- arithmetic-calculator/src/main/java/org/example/Calculator.java
+++ arithmetic-calculator/src/main/java/org/example/Calculator.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.calculate.*;
+
+import java.util.List;
+
 public class Calculator {
-    public static int calculate(int operand1, String operator, int operand2) {
-        return ArithmeticOperator.calculate(operand1, operator, operand2);
+
+    // 인터페이스의 구현체들을 상위 인터페이스 타입에 리스트를 통해 저장시킨다.
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
+    public static int calculate(PositiveNumber operand1, String operator, PositiveNumber operand2) {
+        return arithmeticOperators.stream()
+                // 1. 해당하는 연산자에 구현체를 찾는다.
+                .filter(arithmeticOperator -> arithmeticOperator.supports(operator))
+                // 2. 해당 구현체의 calculate 메소드에 위임한다.
+                .map(arithmeticOperator -> arithmeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
 }
