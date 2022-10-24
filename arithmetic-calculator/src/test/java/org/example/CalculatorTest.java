6de08780d53e2ea6e3312b38a73245bda0d7e8b1
bang-star <bang77060986@gmail.com>
2022-10-25 00:45:40 +0900

    Update  - 요구사항 2, 3번에 대한 테스트 코드 추가

--- arithmetic-calculator/src/test/java/org/example/CalculatorTest.java
+++ arithmetic-calculator/src/test/java/org/example/CalculatorTest.java
@@ -7,7 +7,9 @@ package org.example;
 * MVC 패턴(Model-View-Controller) 기반으로 구현한다.
 * */
 
+import org.example.calculate.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,6 +17,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class CalculatorTest {
 
@@ -23,7 +26,7 @@ public class CalculatorTest {
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void additionTest(int operand1, String operator, int operand2, int result) {
-        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+        int calculateResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
         assertThat(result).isEqualTo(calculateResult);
     }
 
@@ -34,5 +37,12 @@ public class CalculatorTest {
                 Arguments.arguments(2, "*", 3, 6),
                 Arguments.arguments(4, "/", 2, 2)
         );
+    }
+
+    @DisplayName("나눗셈에서 0을 나누는 경우 illegalArgument 예외를 발생시킨다.")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(() -> Calculator.calculate(new PositiveNumber(10), "/", new PositiveNumber(0)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
