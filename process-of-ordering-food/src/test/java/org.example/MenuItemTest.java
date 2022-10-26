42d06a3718e9e5b6c4063800ed89a1bf2a545a44
bang-star <bang77060986@gmail.com>
2022-10-27 02:07:53 +0900

    Create - Test 클래스

--- process-of-ordering-food/src/test/java/org.example/MenuItemTest.java
+++ process-of-ordering-food/src/test/java/org.example/MenuItemTest.java
@@ -1,2 +1,17 @@
-package org.example;public class MenuItemTest {
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class MenuItemTest {
+
+    @DisplayName("메뉴 항목을 생성한다.")
+    @Test
+    public void createTest() {
+
+        assertThatCode(() -> new MenuItem("만두", 5000))
+                .doesNotThrowAnyException();
+    }
 }
