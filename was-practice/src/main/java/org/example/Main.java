efad9388375eede02b8d5a7227580a8b3163dc8d
bang-star <bang77060986@gmail.com>
2022-11-01 00:59:15 +0900

    WAS 실행 코드

--- was-practice/src/main/java/org/example/Main.java
+++ was-practice/src/main/java/org/example/Main.java
@@ -1,2 +1,9 @@
-package org.example.calculator;public class Main {
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
+    }
 }
