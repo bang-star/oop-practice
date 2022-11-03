2ed7176adff93c012ecda17ef44e371b3e2ed6b4
bang-star <bang77060986@gmail.com>
2022-11-04 01:10:40 +0900

    CalculatorServlet에 Service메소드 구체화

--- servlet-calculate/src/main/java/org/example/CalculatorServlet.java
+++ servlet-calculate/src/main/java/org/example/CalculatorServlet.java
@@ -24,12 +24,16 @@ public class CalculatorServlet implements Servlet {
     @Override
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         log.info("service");
+
+        // 클라이언트에서 서버로 operand1, operator, operand2 파라미터를 던져서
         int operand1 = Integer.parseInt(request.getParameter("operand1"));
         String operator = request.getParameter("operator");
         int operand2 = Integer.parseInt(request.getParameter("operand2"));
 
+        // 해당 도메인을 이용해서 계산을 시키고
         int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
+        // 결과값을 클라이언트에게 전달
         PrintWriter writer = response.getWriter();
         writer.println(result);
     }
