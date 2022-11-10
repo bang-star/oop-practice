8cb6a3279a9a5b38c198a362a161ec20bf80db9e
bang-star <bang77060986@gmail.com>
2022-11-10 15:09:43 +0900

    feat : DB 커넥션 풀 적용 및 JDBC를 이용하여 간단한 CRUD 리팩토링

--- jdbc-practice/src/main/java/org/example/ConnectionManager.java
+++ jdbc-practice/src/main/java/org/example/ConnectionManager.java
@@ -3,16 +3,47 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static DataSource getDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setPassword("");
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String DB_USERNAME = "sa";
+    private static final String DB_PASSWORD = "";
+    public static final int MAX_POOL_SIZE = 40;
 
-        return hikariDataSource;
+    private static final DataSource ds;
+
+    static {
+        // DataSource는 하나이기 때문에 static 으로 초기화
+
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(DB_USERNAME);
+        hikariDataSource.setPassword(DB_PASSWORD);
+
+        // 커넥션 풀의 최소와 최대 개수 설정
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
     }
+
+
+    // 커넥션 Pool 적용
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static DataSource getDataSource(){
+        return ds;
+    }
+
 }
--- jdbc-practice/src/main/java/org/example/UserDao.java
+++ jdbc-practice/src/main/java/org/example/UserDao.java
@@ -4,75 +4,92 @@ import java.sql.*;
 
 public class UserDao {
 
-    private Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
+    // AS-IS
+//    public void create(User user) throws SQLException{
+//        Connection con = null;
+//        PreparedStatement pstmt = null;
+//
+//        try{
+//            con = ConnectionManager.getConnection();
+//            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";   // 변경될 수 있는 요소
+//
+//            pstmt = con.prepareStatement(sql);
+//
+//            pstmt.setString(1, user.getUserId());
+//            pstmt.setString(2, user.getPassword());
+//            pstmt.setString(3, user.getName());
+//            pstmt.setString(4, user.getEmail());
+//
+//            pstmt.executeUpdate();
+//        }finally {
+//            if (pstmt != null)
+//                pstmt.close();
+//
+//            if (con != null)
+//                con.close();
+//        }
+//    }
 
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
+    // TO-BE
     public void create(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-
-            pstmt = con.prepareStatement(sql);
-
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.executeUpdate(user, sql, pstmt -> {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
-
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null)
-                pstmt.close();
-
-            if (con != null)
-                con.close();
-        }
+        });
     }
 
+    // AS-IS
+//    public User findByUserId(String userId) throws SQLException {
+//        Connection con = null;
+//        PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//
+//        try {
+//            con = ConnectionManager.getConnection();
+//            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid = ?";
+//            pstmt = con.prepareStatement(sql);
+//            pstmt.setString(1, userId);
+//            rs = pstmt.executeQuery();
+//
+//            User user = null;
+//            if (rs.next()) {
+//                user = new User(
+//                        rs.getString("userId"),
+//                        rs.getString("password"),
+//                        rs.getString("name"),
+//                        rs.getString("email"));
+//            }
+//
+//            return user;
+//        } finally {
+//            if (rs != null)
+//                rs.close();
+//
+//            if (pstmt != null)
+//                pstmt.close();
+//
+//            if (con != null)
+//                con.close();
+//        }
+//    }
+
+    // TO-BE : 변경되는 부분은 API 호출자 입장에서 변경하도록 하면 된다.
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid = ?";
 
-        try {
-            con = getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid = ?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-            rs = pstmt.executeQuery();
+        return (User) jdbcTemplate.executeQuery(sql,
+                pstmt -> pstmt.setString(1, userId),
 
-            User user = null;
-            if (rs.next()) {
-                user = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null)
-                rs.close();
-
-            if (pstmt != null)
-                pstmt.close();
-
-            if (con != null)
-                con.close();
-        }
+                resultSet -> new User(
+                        resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")
+                ));
     }
 }
