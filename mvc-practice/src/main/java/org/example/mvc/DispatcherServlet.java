25962facd82365bea308990e82893b0c3bde6838
bang-star <bang77060986@gmail.com>
2022-11-14 22:01:37 +0900

    어노테이션 기반 핸들러 추가

--- mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
+++ mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,6 +1,5 @@
 package org.example.mvc;
 
-import org.example.mvc.controller.Controller;
 import org.example.mvc.controller.RequestMethod;
 import org.example.mvc.view.JSPViewResolver;
 import org.example.mvc.view.ModelAndView;
@@ -23,7 +22,7 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
-    private HandlerMapping handlerMapping;          // 구체적인 클래스 타입이 아닌 인터페이스 타입으로 선언
+    private List<HandlerMapping> handlerMappings;          // 구체적인 클래스 타입이 아닌 인터페이스 타입으로 선언
 
     private List<HandlerAdapter> handlerAdapters;
 
@@ -34,9 +33,12 @@ public class DispatcherServlet extends HttpServlet {
         RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
         rmhm.init();
 
-        handlerMapping = rmhm;
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("org.example");
+        ahm.initialize();
 
-        handlerAdapters = List.of(new SimpleControllerHandlaerAdapter());
+        handlerMappings = List.of(rmhm, ahm);
+
+        handlerAdapters = List.of(new SimpleControllerHandlaerAdapter(), new AnnotationHandlerAdapter());
         viewResolvers = Collections.singletonList(new JSPViewResolver());
     }
 
@@ -45,17 +47,17 @@ public class DispatcherServlet extends HttpServlet {
         // 모든 경로에 대해 호출되는지 판단하기 위한 로그
         log.info("[DispatcherServlet] service started");
 
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
         try {
             // Handler Mapping
-            Object handler = handlerMapping.findHanlder(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));     // 1. 요청된 경로를 통해 적절한 컨트롤러 반환
+            Object handler = handlerMappings.stream()
+                    .filter(hm -> hm.findHanlder(new HandlerKey(requestMethod, requestURI)) != null)
+                    .map(hm -> hm.findHanlder(new HandlerKey(requestMethod, requestURI)))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No Handler for [" + requestMethod + "," + requestURI + "]"));
 
-            // String viewName = handler.handleRequest(request, response);         // 2. Handler에게 작업을 위임하고 해당 viewName을 받는다.
-
-            // AS-IS : forward만 가능한 코드
-            // RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            // requestDispatcher.forward(request, response);
-
-            // TO-BE : HandlerAdapater와 ViewResolver 적용
 
             // HandlerAdapater
             HandlerAdapter handlerAdapter = handlerAdapters.stream()
@@ -64,6 +66,7 @@ public class DispatcherServlet extends HttpServlet {
                     .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
 
             ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
+            
 
             // ViewResolver
             for (ViewResolver viewResolver : viewResolvers) {
--- mvc-practice/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ mvc-practice/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -12,7 +12,7 @@ public class RequestMappingHandlerMapping implements HandlerMapping {
 
     // 인덱스 페이지에 대한 설정
     void init(){
-        mappings.put(new HandlerKey(RequestMethod.GET, "/"), new HomeController());
+//        mappings.put(new HandlerKey(RequestMethod.GET, "/"), new HomeController());
         mappings.put(new HandlerKey(RequestMethod.GET, "/users"), new UserListController());
         mappings.put(new HandlerKey(RequestMethod.POST, "/users"), new UserCreateController());
         mappings.put(new HandlerKey(RequestMethod.GET, "/user/form"), new ForwardController("/user/form"));
