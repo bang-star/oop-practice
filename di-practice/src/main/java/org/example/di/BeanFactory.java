1706d89469a0702cd1d8c6b08b2e801475731992
bang-star <bang77060986@gmail.com>
2022-11-21 16:01:59 +0900

    findConstructor 메소드를 유용하게 사용할 수 있도록 리팩토링

--- di-practice/src/main/java/org/example/di/BeanFactory.java
+++ di-practice/src/main/java/org/example/di/BeanFactory.java
@@ -47,15 +47,13 @@ public class BeanFactory {
     }
 
     private Constructor<?> findConstructor(Class<?> clazz) {
-        // 클래스 타입 객체에 모든 생성자를 가져온다. 단, Inject 어노테이션이 붙은 생성자에 한해서
-        Set<Constructor> injectedConstructors = ReflectionUtils.getAllConstructors(clazz, ReflectionUtils.withAnnotation(Inject.class));
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(clazz);
 
-        if(injectedConstructors.isEmpty()){
-            return null;
+        if (Objects.nonNull(constructor)) {
+            return constructor;
         }
 
-        // 일단 첫번째 요소만 반환
-        return injectedConstructors.iterator().next();
+        return clazz.getConstructors()[0];
     }
 
 
