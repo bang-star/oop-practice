d7ed315e929c99a5f74ed55876421579b48cd72f
bang-star <bang77060986@gmail.com>
2022-11-21 15:57:03 +0900

    어노테이션이 붙은 클래스 저장하는 initialize() 구현
    - createInstance() : 해당 클래스에 대한 인스턴스를 생성하는 메소드
    - findConstructor() : Inject 어노테이션이 붙은 Class 타입 객체의 모든 생성자를 가져와서 반환하는 findConstructor 구현

--- di-practice/src/main/java/org/example/di/BeanFactory.java
+++ di-practice/src/main/java/org/example/di/BeanFactory.java
@@ -1,8 +1,12 @@
 package org.example.di;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import org.example.annotation.Inject;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class BeanFactory {
 
@@ -15,6 +19,45 @@ public class BeanFactory {
         this.preInstantiatedClazz = preInstantiatedClazz;
         initialize();
     }
+
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClazz) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        // 생성자
+        Constructor<?> constructor = findConstructor(clazz);
+
+        // 파라미터 정보
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()) {
+            // 파라미터 타입에 대한 클래스 타입 객체를 가져와서 클래스 타입 객체에 대한 인스턴스를 가져오는 메소드이다.
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // 인스턴스 생성
+        try {
+            return constructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz) {
+        // 클래스 타입 객체에 모든 생성자를 가져온다. 단, Inject 어노테이션이 붙은 생성자에 한해서
+        Set<Constructor> injectedConstructors = ReflectionUtils.getAllConstructors(clazz, ReflectionUtils.withAnnotation(Inject.class));
+
+        if(injectedConstructors.isEmpty()){
+            return null;
+        }
+
+        // 일단 첫번째 요소만 반환
+        return injectedConstructors.iterator().next();
+    }
+
 
     // 어떤 타입의 파라미터가 인자로 들어올지 알수 없기 때문에 제네릭 타입으로 선언
     public <T> T getBean(Class<T> requiredType) {
