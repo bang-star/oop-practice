73ab1cc1389266b3b8bcd5f771fd88e7bd2a298e
bang-star <bang77060986@gmail.com>
2022-11-21 15:33:22 +0900

    BeanFactory에 클래스 타입을 입력하면 해당 클래스 객체의 인스턴스를 반환하도록 구현

--- di-practice/src/main/java/org/example/di/BeanFactory.java
+++ di-practice/src/main/java/org/example/di/BeanFactory.java
@@ -1,5 +1,24 @@
 package org.example.di;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class BeanFactory {
 
+    private final Set<Class<?>> preInstantiatedClazz;
+
+    // [Key] 클래스 타입 객체, [Value] 클래스 타입 객체의 인스턴스
+    private Map<Class<?>, Object> beans = new HashMap<>();
+
+    public BeanFactory(Set<Class<?>> preInstantiatedClazz) {
+        this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize();
+    }
+
+    // 어떤 타입의 파라미터가 인자로 들어올지 알수 없기 때문에 제네릭 타입으로 선언
+    public <T> T getBean(Class<T> requiredType) {
+        // 해당 인스턴스 변수에서 요구된 클래스 타입 객체의 인스턴스를 반환
+        return (T) beans.get(requiredType);
+    }
 }
